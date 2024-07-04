ad0024bd7f7206c45b705cd196389ab7c34aad4c
Romain Hernandez <romsher.dev@gmail.com>
2024-07-04 12:32:16 +0200

    feat: add greenButton

---
+++ app/src/components/Main/ButtonGreen.jsx
@@ -0,0 +1,8 @@
+import Link from "next/link"
+export default function ButtonCover({title, url}){
+  return(
+    <button className=" text-contrast-1 py-1 px-4 rounded-xl bg-solid text-sm hover:text-contrast-3 transition-all">
+    <Link href={url} className="font-bold">{title}</Link>
+    </button>
+  )
+}
