9ac81163aa74b09c0ff7fa3ef8db33ca941c56f4
Romain Hernandez <romsher.dev@gmail.com>
2024-07-16 23:07:23 +0200

    style: add inline-block on name for ArticleTournament

--- app/src/components/Main/Tournaments/ArticleTournament.jsx
+++ app/src/components/Main/Tournaments/ArticleTournament.jsx
@@ -23,10 +23,10 @@ export default function AticleTournaments({ trophy }) {
 
             <div class="flex-auto">
               <p class="text-lg md:text-xl">{winner.year}</p>
-              <div className="flex flex-col md:flex-row justify-between">
-                <p class="font-bold md:text-lg">Vainceur : {winner.name}</p>
+              <div className="flex flex-col md:flex-row justify-between gap-4">
+                <p class="font-bold md:text-lg">Vainceur : <span className="inline-block">{winner.name}</span></p>
                 {winner.vs && (
-                  <p class="font-bold md:text-lg">Adversaire : {winner.vs}</p>
+                  <p class="font-bold md:text-lg">Adversaire : <span className="inline-block">{winner.vs}</span></p>
                 )}
               </div>
             </div>
