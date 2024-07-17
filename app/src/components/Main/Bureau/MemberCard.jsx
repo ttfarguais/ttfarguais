139ff648f6e3064ce3b0f6387d9c7409d9e8897c
Romain Hernandez <romsher.dev@gmail.com>
2024-07-17 18:05:32 +0200

    remove membres picture with bg-green  where membre.img is not defined

--- app/src/components/Main/Bureau/MemberCard.jsx
+++ app/src/components/Main/Bureau/MemberCard.jsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 export default function MemberCard({membre}){
   return (
     <article className="flex flex-col gap-4 bg-contrast-2 rounded-xl shadow p-10 w-50">
-    <Image src={membre.img}
+    {membre.img ? (<Image src={membre.img}
     width={100}
     height={100}
     alt={membre.alt}
-    className="m-auto w-40 h-40 object-cover object-top rounded-full"/>
+    className="m-auto w-40 h-40 object-cover object-top rounded-full"/>) : <div className="m-auto w-40 h-40 bg-emerald-100 rounded-full"></div>}
     <strong className="text-center text-lg text-solid font-extrabold">{membre.lastname} {membre.firstname}</strong>
     <p className="text-center text-sm">{membre.job}</p>
   </article>
