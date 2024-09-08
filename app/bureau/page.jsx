c3e6c564b94e20ca5eb3e98998c0b05265102048
Romain Hernandez <romsher.dev@gmail.com>
2024-09-08 12:44:02 +0200

    fix: extension picture

--- app/bureau/page.jsx
+++ app/bureau/page.jsx
@@ -8,13 +8,13 @@ export const metadata = {
 export default function BureauPage(){
 
   const membres = [
-    {firstname:"Jeremy", lastname:"THEVENET", job:"Président",img:"/membres/Jeremy.jpg", alt: "Photo de Jeremy Thevenet"},
-    {firstname:"Hugues", lastname:"CHAN-NG-YOK", job:"Vice-Président", alt: "Photo de Jeremy Thevenet"},
+    {firstname:"Jeremy", lastname:"THEVENET", job:"Président",img:"/membres/Jeremy.JPG", alt: "Photo de Jeremy Thevenet"},
+    {firstname:"Hugues", lastname:"CHAN-NG-YOK", job:"Vice-Président", alt: "Photo de Hugues Chan-Ng-Yok"},
     {firstname:"Patrick", lastname:"MARTINEAU", job:"Trésorier", alt: "Photo de Patrick Martineau"},
     {firstname:"Stéphane", lastname:"AIMEE", job:"Secrétaire", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
-    {firstname:"Agostinho José", lastname:"DUARTE", job:"Secrétaire-adjoint",img:"/membres/Jose.jpg", alt: "Photo de Stéphane Aimee"},
+    {firstname:"Agostinho José", lastname:"DUARTE", job:"Secrétaire-adjoint",img:"/membres/JOSE.JPG", alt: "Photo de Jose Duarte"},
     {firstname:"Maxime", lastname:"TOURNEAUX", job:"Entaîneur", alt: "Photo de Maxime Tourneaux"},
-    {firstname:"Emmanuel", lastname:"BOS", job:"Media",img:"/membres/Manu.jpg", alt: "Photo de Emmanuel Bos"},
+    {firstname:"Emmanuel", lastname:"BOS", job:"Media",img:"/membres/Manu.JPG", alt: "Photo de Emmanuel Bos"},
   ]
 
   return (
