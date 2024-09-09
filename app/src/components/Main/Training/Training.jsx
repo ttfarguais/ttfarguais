e870eb8b96ca7f5348b192ddb2d414c00224ff6d
Romain Hernandez <romsher.dev@gmail.com>
2024-09-09 10:35:56 +0200

    fix: training time update

--- app/src/components/Main/Training/Training.jsx
+++ app/src/components/Main/Training/Training.jsx
@@ -6,15 +6,15 @@ export default function Training() {
       name: "Adultes",
       schedules: [
         { day: "Lundi", time: "18h00 - 20h30", who: "Loisirs et tous compétiteurs", trainingType: "Entraînement libre et dirigé", coach: "AIMÉE Stéphane" },
-        { day: "Mercredi", time: "19h00 - 20h30", who: "Loisirs et tous compétiteurs", trainingType: "Entraînement libre" },
+        { day: "Mercredi", time: "19h00 - 21h00", who: "Loisirs et tous compétiteurs", trainingType: "Entraînement libre" },
       ]
     },
     {
       name: "Jeunes",
       schedules: [
-        { day: "Mercredi", time: "14h00 - 15h30", who: "Initiation jeunes des écoles primaires-1ᵉʳ groupe *", trainingType: "Entraînement dirigé - Initiation", coach: "TOURNEAUX Maxime" },
-        { day: "Mercredi", time: "15h30 - 17h30", who: "Initiation jeunes des collèges-Lycées 2ᵉ groupe *", trainingType: "Entraînement dirigé - Débutant", coach: "TOURNEAUX Maxime" },
-        { day: "Mercredi", time: "17h30 - 19h30", who: "Initiation jeunes des collèges-lycées 3ᵉ groupe *", trainingType: "Entraînement dirigé", coach: "TOURNEAUX Maxime" },
+        { day: "Mercredi", time: "14h00 - 15h30", who: "Initiation jeunes des écoles primaires-1ᵉʳ groupe *", trainingType: "Entraînement dirigé - Initiation", coach: "TOURNAUX Maxime" },
+        { day: "Mercredi", time: "15h30 - 17h00", who: "Initiation jeunes des collèges-Lycées 2ᵉ groupe *", trainingType: "Entraînement dirigé - Débutant", coach: "TOURNAUX Maxime" },
+        { day: "Mercredi", time: "17h00 - 19h00", who: "Initiation jeunes des collèges-lycées 3ᵉ groupe *", trainingType: "Entraînement dirigé", coach: "TOURNAUX Maxime" },
       ]
     }
   ];
