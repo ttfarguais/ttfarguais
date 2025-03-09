dd4b77e25fe0a66975021746da148396878e256f
Romain Hernandez <romsher.dev@gmail.com>
2025-03-09 20:47:09 +0100

    fix: extension

--- app/src/utils/separatorUnderscoreString.js
+++ app/src/utils/separatorUnderscoreString.js
@@ -1,3 +1,3 @@
 export default function separatorUderscoreString(str) {
-    return str.split("/").pop().replace(/_/g, " ").slice(0, -4);
+    return str.split("/").pop().replace(/_/g, " ").replace(/\.[^/.]+$/, "");
 }
