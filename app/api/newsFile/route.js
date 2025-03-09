b1e4dbeeff66366b0316fd3a09845d948e541372
Romain Hernandez <romsher.dev@gmail.com>
2025-03-09 20:06:55 +0100

    log

--- app/api/newsFile/route.js
+++ app/api/newsFile/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
     const files = blobs.filter((blob) =>
         blob.pathname.startsWith("actualite/")
     );
-
+    console.log(files)
     const filesWithoutFirst = files.slice(1);
 
     const response = new Response(JSON.stringify(filesWithoutFirst), {
