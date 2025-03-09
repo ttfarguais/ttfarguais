e447d3e5c265cc1b40aeaf428ba0e7f9495d3e01
Romain Hernandez <romsher.dev@gmail.com>
2025-03-09 20:52:26 +0100

    fix: extension

--- app/src/utils/separatorUnderscoreString.js
+++ app/src/utils/separatorUnderscoreString.js
@@ -1,3 +1,3 @@
 export default function separatorUderscoreString(str) {
-    return str.split("/").pop().replace(/_/g, " ").replace(/\.[^/.]+$/, "");
+    return str.split("/").pop().replace(/_/g, " ").replace(/\.(pdf|jpg|jpeg|png|gif|bmp|tiff)$/i, "");
 }
