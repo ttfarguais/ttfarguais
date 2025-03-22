c95fb8ca8e86fce48697f57f2c66ca5fddd5d365
TT Farguais <tenisdetablefarguais@gmail.com>
2025-03-22 02:27:23 +0100

    Update newsArticles.js

--- app/src/data/newsArticles.js
+++ app/src/data/newsArticles.js
@@ -1,7 +1,7 @@
 const newsArticles = [
   {url: "/article/actu", alt: "Actualité club de tennis de table Farguais", title: "la refonte graphique", description: "Le T.T. Farguais se met à jour ! Un changement important au sein de notre club trentenaire.", src:"/imgClub/raquette.jpg"},
   {url: "/inscription", alt: "Club de tennis de table Farguais", title: "Nouvelle Saison 2024/2025", description: "Inscrivez-vous dès maintenant et rejoignez le T.T. Farguais !", src:"/imgClub/club.jpg"},
-  {url: "/article/evenements/anniversaire", alt: "30 ans d'histoire", title: "30 ans d'histoire", description: "2022, déjà 30 ans que le T.T. Farguais existe, alors nous nous devons de fêter ça autour de quelques ping et deux, trois pong.", src:"/imgClub/birthday.jpg"},
+  {url: "/article/evenements/anniversaire", alt: "30 ans d'histoire", title: "30 ans d'histoire", description: "Déjà 30 ans que le T.T. Farguais existe, alors nous nous devons de fêter ça autour de quelques ping et deux, trois pong.", src:"/imgClub/birthday.jpg"},
 ]
 
 export default newsArticles;
