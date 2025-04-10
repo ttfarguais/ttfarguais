a68509b005fac45c89db78130cc63ca180daaa4c
TT Farguais <tenisdetablefarguais@gmail.com>
2025-04-11 01:47:09 +0200

    Update page.jsx

--- app/entrainements/page.jsx
+++ app/entrainements/page.jsx
@@ -3,7 +3,7 @@ import Training from "../src/components/Main/Training/Training";
 export const metadata = {
   title: "T.T. Farguais - Entrainements",
   description:
-    "Rejoignez le club de tennis de table de Fargues Saint Hilaire. Retrouvez toutes les informations relatives aux entrainements.",
+    "Rejoignez le club de tennis de table de Fargues Saint-Hilaire. Retrouvez toutes les informations relatives aux entrainements.",
 };
 
 export default function TrainingPage() {
