ea15d6a568398bf94b1e8e96eb857a95ec95c48e
TT Farguais <tenisdetablefarguais@gmail.com>
2025-04-30 02:15:55 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -10,9 +10,9 @@ const trainingTimes = [
     name: "Jeunes",
     schedules: [
 
-      { day: "Mercredi", time: "14h00 - 15h30", who: "Initiation jeunes des écoles primaires 1ᵉʳ groupe *", trainingType: "Entraînement dirigé - Initiation", coach: "TOURNAUX Maxime" },
-      { day: "Mercredi", time: "15h30 - 17h00", who: "Initiation jeunes des collèges-lycées 2ᵉ groupe *", trainingType: "Entraînement dirigé - Débutant", coach: "TOURNAUX Maxime" },
-      { day: "Mercredi", time: "17h00 - 19h00", who: "Initiation jeunes des collèges-lycées 3ᵉ groupe *", trainingType: "Entraînement dirigé - Confirmés", coach: "TOURNAUX Maxime" },
+      { day: "Mercredi", time: "14h00 - 15h30", who: "Initiation jeunes des écoles primaires : 1ᵉʳ groupe *", trainingType: "Entraînement dirigé - Initiation", coach: "TOURNAUX Maxime" },
+      { day: "Mercredi", time: "15h30 - 17h00", who: "Initiation jeunes des collèges-lycées : 2ᵉ groupe *", trainingType: "Entraînement dirigé - Débutant", coach: "TOURNAUX Maxime" },
+      { day: "Mercredi", time: "17h00 - 19h00", who: "Initiation jeunes des collèges-lycées : 3ᵉ groupe *", trainingType: "Entraînement dirigé - Confirmés", coach: "TOURNAUX Maxime" },
     ]
   }
 ];
