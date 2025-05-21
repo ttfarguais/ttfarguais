638dbb2e4b251b11801515cba2a1f2863d87c9f3
TT Farguais <tenisdetablefarguais@gmail.com>
2025-05-22 01:31:44 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -1,9 +1,9 @@
-ntraîneurconst clubMembres = [
+const clubMembres = [
   {firstname:"Jean-Paul", lastname:"VERGOTE", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
   {firstname:"Patrick", lastname:"MARTINEAU", job:"Trésorier", img:"/membres/Patrick.png", alt: "Photo de Patrick Martineau"},
   {firstname:"Denis", lastname:"GRENIER", job:"Membre du Comité directeur", img:"/membres/Denis.png", alt: "Photo de Denis Grenier"},
   {firstname:"Philippe", lastname:"VANBAUCE", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
-  {firstname:"Stéphane", lastname:"AIMÉE", job:"Secrétaire entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
+  {firstname:"Stéphane", lastname:"AIMÉE", job:"Secrétaire Entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
   {firstname:"Agostinho José", lastname:"DUARTE", job:"Secrétaire adjoint", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
   {firstname:"Maxime", lastname:"TOURNAUX", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
   {firstname:"Emmanuel", lastname:"BOS", job:"Réseaux sociaux", img:"/membres/Manu.jpg", alt: "Photo de Emmanuel Bos"},
