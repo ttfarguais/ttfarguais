a2e135c16fddc4fd452f7cd778974728134e4c1b
TT Farguais <tenisdetablefarguais@gmail.com>
2025-09-18 01:42:08 +0200

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,11 +4,16 @@ export const metadata = {
   title: "T.T. Farguais - Contact",
   description:
     "Contactez le club de tennis de table de Fargues Saint Hilaire. Nous vous répondrons dans les plus brefs délais.",
-  alternates: {
-    canonical: "https://www.ttfarguais.fr/contact",
-  },
 };
 
 export default function ContactPage() {
-  return <ContactContainer />;
+ 
+  return (
+    <>
+    <head>
+    <link rel="canonical" href="https://www.ttfarguais.fr/contact" />
+    </head>
+    <ContactContainer></ContactContainer>
+    </>
+  );
 }
