601bbe5b2ef71900f79c22016f280b5a1ddd8512
TT Farguais <tenisdetablefarguais@gmail.com>
2025-10-09 00:04:46 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -1,11 +1,11 @@
 const clubMembres = [
   {firstname:"Jean-Paul", lastname:"VERGOTE", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
   {firstname:"Denis", lastname:"GRENIER", job:"Trésorier", img:"/membres/Denis.png", alt: "Photo de Denis Grenier"},
-  {firstname:"Patrick", lastname:"MARTINEAU", job:"Membre du Comité directeur", img:"/membres/Patrick.png", alt: "Photo de Patrick Martineau"},
-  {firstname:"Agostinho José", lastname:"DUARTE", job:"Communication - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
   {firstname:"Stéphane", lastname:"AIMÉE", job:"Secrétaire - Entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
-  {firstname:"Philippe", lastname:"VANBAUCE", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
+  {firstname:"Agostinho José", lastname:"DUARTE", job:"Communication - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
   {firstname:"Maxime", lastname:"TOURNAUX", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
+  {firstname:"Philippe", lastname:"VANBAUCE", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
+  {firstname:"Patrick", lastname:"MARTINEAU", job:"Membre du Comité directeur", img:"/membres/Patrick.png", alt: "Photo de Patrick Martineau"},
   {firstname:"Emmanuel", lastname:"BOS", job:"Réseaux sociaux", img:"/membres/Manu.jpg", alt: "Photo de Emmanuel Bos"},
 ]
 
