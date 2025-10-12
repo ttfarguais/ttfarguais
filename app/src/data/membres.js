ae44714f4ca82e52c0f3311dac94470ad91ca932
TT Farguais <tenisdetablefarguais@gmail.com>
2025-10-13 00:18:51 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -1,13 +1,14 @@
 const clubMembres = [
-  {firstname:"Jean-Paul", lastname:"VERGOTE", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
-  {firstname:"Denis", lastname:"GRENIER", job:"Trésorier", img:"/membres/Denis2.png", alt: "Photo de Denis Grenier"},
-  {firstname:"Stéphane", lastname:"AIMÉE", job:"Secrétaire - Entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
-  {firstname:"Maxime", lastname:"TOURNAUX", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
-  {firstname:"Anne Sophie", lastname:"GARNIER", job:"Réseaux sociaux", img:"/membres/ANNSO.jpg", alt: "Photo de Anne Sophie"},
-  {firstname:"Agostinho José", lastname:"DUARTE", job:"Coordinateur - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
-  {firstname:"Jérémy", lastname:"THEVENET", job:"Membre du Comité directeur", img:"/membres/Jeremy.jpg", alt: "PHOTO"},
-  {firstname:"Philippe", lastname:"VANBAUCE", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
-  {firstname:"David", lastname:"TAUZIA", job:"Membre du Comité directeur", img:"/membres/David.jpg", alt: "PHOTO"},
+  {firstname:"VERGOTE", lastname:"Jean-Paul", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
+  {firstname:"GRENIER", lastname:"Denis", job:"Trésorier", img:"/membres/Denis2.png", alt: "Photo de Denis Grenier"},
+  {firstname:"AIMÉE", lastname:"Stéphane", job:"Secrétaire - Entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
+  {firstname:"TOURNAUX", lastname:"Maxime", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
+  {firstname:"GARNIER", lastname:"Anne Sophie", job:"Réseaux sociaux", img:"/membres/ANNSO.jpg", alt: "Photo de Anne Sophie"},
+  {firstname:"DUARTE", lastname:"Agostinho José", job:"Coordinateur - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
+  {firstname:"THEVENET", lastname:"Jérémy", job:"Membre du Comité directeur", img:"/membres/Jeremy.jpg", alt: "PHOTO"},
+  {firstname:"TAUZIA", lastname:"David", job:"Membre du Comité directeur", img:"/membres/David.jpg", alt: "PHOTO"},
+  {firstname:"VANBAUCE", lastname:"Philippe", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
+
 ]
 
 export default clubMembres;
