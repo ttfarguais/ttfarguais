272cfd62fdd3d2a6c45457043b1708a03fbf7c4b
TT Farguais <tenisdetablefarguais@gmail.com>
2025-10-13 00:10:03 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -5,7 +5,6 @@ const clubMembres = [
   {firstname:"Agostinho José", lastname:"DUARTE", job:"Coordinateur - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
   {firstname:"Maxime", lastname:"TOURNAUX", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
   {firstname:"Anne Sophie", lastname:"GARNIER", job:"Réseaux sociaux", img:"/membres/ANNSO.jpg", alt: "Photo de Anne Sophie"},
-  {firstname:"Patrick", lastname:"MARTINEAU", job:"Membre du Comité directeur", img:"/membres/Patrick.png", alt: "Photo de Patrick Martineau"},
   {firstname:"Jérémy", lastname:"THEVENET", job:"Membre du Comité directeur", img:"/membres/Jeremy.jpg", alt: "PHOTO"},
   {firstname:"Philippe", lastname:"VANBAUCE", job:"Membre du Comité directeur", img:"/membres/Philippe.png", alt: "Photo de Philippe Vanbauce"},
   {firstname:"David", lastname:"TAUZIA", job:"Membre du Comité directeur", img:"/membres/David.jpg", alt: "PHOTO"},
