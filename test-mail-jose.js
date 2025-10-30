047f6a8fa986ef88c3cb8d28bbdd3de982888124
TT Farguais <tenisdetablefarguais@gmail.com>
2025-10-30 22:14:58 +0100

    Create test-mail-jose.js

---
+++ test-mail-jose.js
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+import "dotenv/config"; // pour charger ton .env.local
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", // change selon ton service
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ Erreur de connexion SMTP :", error);
+  } else {
+    console.log("✅ Connexion SMTP réussie !");
+  }
+});
