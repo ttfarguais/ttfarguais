e19d11c9da7cedfbc1246a7d1a1eba448e9c2e77
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-18 00:20:14 +0200

    Update Footer-Adress.jsx

--- app/src/components/Footer/Footer-Adress.jsx
+++ app/src/components/Footer/Footer-Adress.jsx
@@ -2,11 +2,10 @@ export default function FooterAdress() {
   return (
     <div className="text-xs sm:text-sm">
       <p>Carré des Forges</p>
+      <p>Derrière la Mairie</p>
       <p>3, av. de La Laurence</p>
       <p>
-      33370
-      </p>
-         <p className="uppercase">Fargues Saint-Hilaire</p>
+      33370 FARGUES ST-HILAIRE</p>
     </div>
   );
 }
