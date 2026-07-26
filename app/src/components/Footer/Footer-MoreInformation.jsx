170e6cedf469e99822b199cb5aaca9794bfefa7e
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-26 23:14:03 +0200

    Update Footer-MoreInformation.jsx

--- app/src/components/Footer/Footer-MoreInformation.jsx
+++ app/src/components/Footer/Footer-MoreInformation.jsx
@@ -22,7 +22,7 @@ export default function FooterMoreInformation() {
           className="italic"
         >
           Barenko, 
-        </Link> maintenu par José
+        </Link> maintenu par <italic>José</italic>
       </p>
     </div>
   );
