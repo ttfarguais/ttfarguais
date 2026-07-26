f447eac5eaa4b79f4b1dec79b759b2ba8b4f8c99
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-26 23:35:19 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -1,10 +1,10 @@
 const clubMembres = [
   {firstname:"VERGOTE", lastname:"Jean-Paul", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
-  {firstname:"Mélanie", lastname:"Bientôt", job:"Trésorière", img:"/membres/Denis2.png", alt: "Photo"},
+  {firstname:"AIMÉE", lastname:"Mélanie", job:"Trésorière", img:"/membres/Denis2.png", alt: "Photo"},
   {firstname:"AIMÉE", lastname:"Stéphane", job:"Secrétaire - Entraîneur", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
   {firstname:"TOURNAUX", lastname:"Maxime", job:"Entaîneur Jeunes", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
   {firstname:"GARNIER", lastname:"Anne Sophie", job:"Réseaux Sociaux", img:"/membres/ANNSO.jpg", alt: "Photo de Anne Sophie"},
-  {firstname:"DUARTE", lastname:"Agostinho José", job:"Coordinateur - Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
+  {firstname:"DUARTE", lastname:"Agostinho José", job:"Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
   {firstname:"THEVENET", lastname:"Jérémy", job:"Membre du Comité directeur", img:"/membres/Jeremy.jpg", alt: "PHOTO"},
   {firstname:"PIKACHU", lastname:"", job:"Mutation en cours", img:"/membres/PIKA.jpg", alt: "PHOTO"},
   {firstname:"CARAPUCE", lastname:"", job:"En cours de dressage", img:"/membres/CARAPUCE.jpg", alt: "Photo"},
