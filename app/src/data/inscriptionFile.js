268d77d632359e740a84d131f47d6a3b2f4067f6
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-27 01:48:34 +0200

    Update inscriptionFile.js

--- app/src/data/inscriptionFile.js
+++ app/src/data/inscriptionFile.js
@@ -1,18 +1,18 @@
 const times = [
   {day: "Lundi", time: "18h00 - 20h30"},
-  {day: "Mercredi", time: "19h00 - 21h00"},
+  {day: "Mercredi", time: "14h00 - 21h00"},
 ]
 
 const documents = [
   { name: "Bordereau de demande de licence", pdf: "/pdf/licence.pdf" },
   { name: "Fiche de renseignements jeune", pdf: "/pdf/TTF-Fiche-renseignements-jeunes.pdf" },
   {
-    name: "Certificat médical pour la pratique du tennis de table (si plus de 5 ans)",
+    name: "Certificat médical pour la pratique du Tennis de Table",
     important: false,
     pdf: "/pdf/certificat-medical.pdf",
   },
-  { name: "Questionnaire de Santé pour personne Majeure (2 années consécutives sur 3)", pdf: "/pdf/autoquestionnaire-medical-majeur.pdf" },
-  { name: "Questionnaire de Santé pour personne Mineure (2 années consécutives sur 3)", pdf: "/pdf/autoquestionnaire-medical-mineur.pdf" },
+  { name: "Questionnaire de Santé pour personne Majeure", pdf: "/pdf/autoquestionnaire-medical-majeur.pdf" },
+  { name: "Questionnaire de Santé pour personne Mineure", pdf: "/pdf/autoquestionnaire-medical-mineur.pdf" },
 ];
 
 const inscriptionFile = {
