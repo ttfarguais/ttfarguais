0b4aa5f34a421e536c526f5f0d323dc3307956fc
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-27 23:51:16 +0200

    Update Training.jsx

--- app/src/components/Main/Training/Training.jsx
+++ app/src/components/Main/Training/Training.jsx
@@ -5,7 +5,7 @@ export default function Training() {
   return (
     <>
    <section className="p-6 md:w-4/6 m-auto 2xl:w-3/6">
-    <h1 className="text-xl text-center font-bold p-4 mb-8">Entraînements</h1>
+    <h1 className="text-xl text-center font-bold p-4 mb-8">ENTRAÎNEMENTS</h1>
 
     <div className="flex flex-col justify-between mb-8 gap-6">
       <ul>
