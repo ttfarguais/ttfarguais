c00506a12c25ca2ceccd2f162a88688073a9e0a3
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-27 02:26:08 +0200

    Update Inscription.jsx

--- app/src/components/Main/Inscription/Inscription.jsx
+++ app/src/components/Main/Inscription/Inscription.jsx
@@ -49,7 +49,7 @@ export default function Inscription({ times, documents, inscriptionFile }) {
         <section className="flex flex-col gap-4 mb-8 ">
           <div>
             <h2 className="mb-8 text-xl font-extrabold text-solid mt-4">
-              Documents à fournir obligatoirement :</h2> Parcours de Prévention Santé (PPS) : https://malicence.fftt.com
+              Documents à fournir obligatoirement :</h2> Parcours de Prévention Santé (PPS) : <h2>https://malicence.fftt.com</h2>
             
             <ul className="flex flex-col gap-6">
               {documents.map((document, index) => (
