cffecdee034140a084509fa2468613b70f1935d9
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-28 21:44:10 +0200

    Update membres.js

--- app/src/data/membres.js
+++ app/src/data/membres.js
@@ -1,7 +1,7 @@
 const clubMembres = [
   {firstname:"VERGOTE", lastname:"Jean-Paul", job:"Président", img:"/membres/JeanPaul.png", alt: "Photo de Jean-Paul Vergote"},
   {firstname:"AIMÉE", lastname:"Mélanie", job:"Trésorière", img:"/membres/ANNSO.jpg", alt: "Photo"},
-  {firstname:"AIMÉE", lastname:"Stéphane", job:"Secrétaire - Juge Arbitre", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
+  {firstname:"AIMÉE", lastname:"Stéphane", job:"Secrétaire", img:"/membres/Stephane.jpg", alt: "Photo de Stéphane Aimee"},
   {firstname:"TOURNAUX", lastname:"Maxime", job:"Entaîneur Comité de Gironde", img:"/membres/MAX.jpg", alt: "Photo de Maxime Tournaux"},
   {firstname:"GARNIER", lastname:"Anne Sophie", job:"Réseaux Sociaux", img:"/membres/ANNSO.jpg", alt: "Photo de Anne Sophie"},
   {firstname:"DUARTE", lastname:"José", job:"Site Internet", img:"/membres/Jose.jpg", alt: "Photo de José Duarte"},
