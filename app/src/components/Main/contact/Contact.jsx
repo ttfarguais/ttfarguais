edbe7eba945eb781ebd04949f5be1b520ca9437c
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-30 00:11:08 +0200

    Update Contact.jsx

--- app/src/components/Main/contact/Contact.jsx
+++ app/src/components/Main/contact/Contact.jsx
@@ -17,8 +17,8 @@ export default function Contact({
       <h1 className="text-xl text-center font-bold p-4 mb-8">
         Contactez-nous !
       </h1>
-      
-        <div><span style="#FF0000">Désolé, le formulaire est temporairement inactif.</span></div>
+      <span style="color: #0000ff>
+        <div>Désolé, le formulaire est temporairement inactif.</span></div>
         <div>Veuillez envoyer votre demande par mail à :</div>
         <div>jeanpaul.vergote@neuf.fr</div>
         <div>Merci et à bientôt.</div>
