a1ff8e0703db6019ff98d92ded5d613771df1ad6
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-31 00:09:03 +0200

    Update Inscription-filesList.jsx

--- app/src/components/Main/Inscription/Inscription-filesList.jsx
+++ app/src/components/Main/Inscription/Inscription-filesList.jsx
@@ -16,3 +16,20 @@ export default function InscriptionilesList({document}) {
   </li>
   )
 }
+
+    <li className="flex items-center justify-between hover:bg-contrast-2 p-2 rounded-xl">
+      <p className="mr-4">{document.name} :</p>
+      <div>
+        <a
+          href={document.pdf}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-solid py-1 px-4 border border-solid rounded-xl hover:text-contrast-1 hover:bg-solid text-sm transition-all"
+          aria-label={`Ouvrir ${document.name}`}
+        >
+          Ouvrir
+        </a>
+      </div>
+    </li>
+  );
+}
