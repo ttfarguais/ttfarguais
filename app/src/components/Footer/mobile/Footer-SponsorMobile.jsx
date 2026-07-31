48a37fd3c4b28877fecfd155375d680b866ba229
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-31 02:34:13 +0200

    Update Footer-SponsorMobile.jsx

--- app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
+++ app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
@@ -1,11 +1,11 @@
-import { FshSponsor, GirondeSponsor, superuSponsor } from "../../UI/Icons";
+import { FshSponsor, GirondeSponsor, SuperuSponsor } from "../../UI/Icons";
 
 export default function FooterSponsor() {
   return (
     <div className="flex gap-6">
      <FshSponsor width={80} height={80}/>
      <GirondeSponsor width={80} height={80}/>
-     <superuSponsor width={80} height={80}/>
+     <SuperuSponsor width={80} height={80}/>
     </div>
   );
 }
