2cfbb957ce4b4b318578670934774fe13bbaad6a
TT Farguais <tenisdetablefarguais@gmail.com>
2026-07-31 22:09:11 +0200

    Update awards.js

--- app/src/data/awards.js
+++ app/src/data/awards.js
@@ -19,7 +19,7 @@ const awards = [
   {date: 2015, title: "Champion de Gironde Division Honneur Minimes Garçons."},
   {date: 2024, title: "Thomas BOS : vainqueur de la Coupe du tournoi du Conseil Général."},
   {date: 2025, title: "Raphaël DUBOUSSET, Elyes AIDARA et Alexandre MARTIN : Vice-Champions de Gironde division Honneur."},
-  {date: 2026, title: "Jean-Paul VERGOTE : Médaille d&apos;Argent du Mérite Régional, sur proposition du Comité de la Gironde et après analyse par la Commission des Mérites de la Ligue
+  {date: 2026, title: "Jean-Paul VERGOTE, Président : Médaille d&apos;Argent du Mérite Régional, sur proposition du Comité de la Gironde et après analyse par la Commission des Mérites de la Ligue
 Nouvelle Aquitaine."},
 ]
 
