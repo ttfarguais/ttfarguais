809c06cb47ddfa4784f5c2a441be2d856bf2e624
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-01 01:21:09 +0200

    Update Index.jsx

--- app/src/components/Footer/Index.jsx
+++ app/src/components/Footer/Index.jsx
@@ -2,8 +2,8 @@ import FooterDesktop from "./desktop/Footer-desktop";
 import FooterMobile from "./mobile/Footer-mobile";
 export default function Index() {
   return (
-    <div>
-      TEST FOOTER
-    </div>
+    <>
+      <FooterMobile />
+    </>
   );
 }
