9544a01f44365cbb2642e998e67718dd8dc5583e
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-01 01:18:55 +0200

    Update Index.jsx

--- app/src/components/Footer/Index.jsx
+++ app/src/components/Footer/Index.jsx
@@ -2,9 +2,8 @@ import FooterDesktop from "./desktop/Footer-desktop";
 import FooterMobile from "./mobile/Footer-mobile";
 export default function Index() {
   return (
-    <>
-      <FooterDesktop />
-      {/* <FooterMobile /> */}
-    </>
+    <div>
+      TEST FOOTER
+    </div>
   );
 }
