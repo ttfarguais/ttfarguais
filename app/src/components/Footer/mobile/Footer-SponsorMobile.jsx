859d8d99bbab1edbe41c53c9c26cb510adcb2fa6
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-01 02:02:14 +0200

    Update Footer-SponsorMobile.jsx

--- app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
+++ app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
@@ -2,10 +2,11 @@ import { FshSponsor, GirondeSponsor, SuperuSponsor } from "../../UI/Icons";
 
 export default function FooterSponsor() {
   return (
-      <div className="flex w-full justify-center items-center gap-1">
-     <FshSponsor width={80} height={80}/>
-     <GirondeSponsor width={70} height={70}/>
-     <SuperuSponsor width={80} height={80}/>
+    <div className="flex w-full flex-wrap justify-center items-center gap-4">
+      <FshSponsor width={80} height={80} />
+      <GirondeSponsor width={80} height={80} />
+      <SuperuSponsor width={80} height={80} />
+      {/* futur 4e logo ici */}
     </div>
   );
 }
