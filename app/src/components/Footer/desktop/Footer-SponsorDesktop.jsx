8b388b906bbb7c2286884f9bd4041aa004ccc158
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-01 02:52:39 +0200

    Update Footer-SponsorDesktop.jsx

--- app/src/components/Footer/desktop/Footer-SponsorDesktop.jsx
+++ app/src/components/Footer/desktop/Footer-SponsorDesktop.jsx
@@ -4,8 +4,9 @@ export default function FooterSponsor() {
   return (
   <div className="flex w-full flex-wrap justify-center items-center gap-x-4 gap-y-4">
       <FshSponsor width={120} height={120} />
+          <SuperuSponsor width={120} height={120} />
       <GirondeSponsor width={120} height={120} />
-      <SuperuSponsor width={120} height={120} />
+
     </div>
   );
 }
