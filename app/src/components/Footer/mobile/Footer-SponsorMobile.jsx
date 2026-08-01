731da8e6f5c9fab1c048253c417e08a43906d6dc
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-01 02:29:29 +0200

    Update Footer-SponsorMobile.jsx

--- app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
+++ app/src/components/Footer/mobile/Footer-SponsorMobile.jsx
@@ -2,7 +2,7 @@ import { FshSponsor, GirondeSponsor, SuperuSponsor } from "../../UI/Icons";
 
 export default function FooterSponsor() {
   return (
-    <div className="flex w-full flex-wrap justify-center items-center gap-4">
+    <div className="flex w-full flex-wrap justify-center items-center gap-x-4 gap-y-1">
       <div className="w-28 h-28">
         <FshSponsor width={120} height={120} />
       </div>
