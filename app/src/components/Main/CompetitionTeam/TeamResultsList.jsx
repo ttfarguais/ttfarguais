207bb32d7a4b7ae96564f868e247c1ef93786d75
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-02 21:03:46 +0200

    Update TeamResultsList.jsx

--- app/src/components/Main/CompetitionTeam/TeamResultsList.jsx
+++ app/src/components/Main/CompetitionTeam/TeamResultsList.jsx
@@ -8,7 +8,7 @@ const ProgressList = ({ progressFiles }) => {
   return (
     <section className="p-6 md:w-4/6 m-auto 2xl:w-3/6 flex flex-col gap-4">
       <h1 className="text-xl text-center font-bold p-4 mb-8">
-      Résultats Compétitions par Équipes
+      Résultats des Compétitions <br />par Équipes
       </h1>
       {progressFiles.competitionFiles.map((files, index) => (
         <div key={index} className="flex flex-col gap-3 border-b mb-10">
