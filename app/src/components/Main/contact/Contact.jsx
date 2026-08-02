fae901b1b0cb390c31896b99d761e8abba667da2
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-02 21:54:12 +0200

    Update Contact.jsx

--- app/src/components/Main/contact/Contact.jsx
+++ app/src/components/Main/contact/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact({
 </p></div>
         <div>À très vite à la table !</div>
         
-{/*      <form
+/*      <form
         onSubmit={handleSubmit}
         className="flex flex-col w-full md:w-4/6 gap-10 py-10 xl:w-3/6 p-6 mb-10"
       >
@@ -213,7 +213,7 @@ export default function Contact({
         </button>
       </form>
     </div>
- */}
+ */
       );
       }
 
