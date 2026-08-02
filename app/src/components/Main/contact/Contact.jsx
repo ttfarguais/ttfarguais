2ee686f73955861764dbe7301efb744a47675839
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-02 22:23:37 +0200

    Update Contact.jsx

--- app/src/components/Main/contact/Contact.jsx
+++ app/src/components/Main/contact/Contact.jsx
@@ -18,7 +18,7 @@ export default function Contact({
         Contactez-nous !
       </h1>
       
-/* 
+      <div>Désolé, le formulaire est temporairement inactif.</div>
       <div>Merci d&apos;envoyer votre demande par mail à notre Président :</div>
         <div className="flex flex-col items-center m-auto py-1">
     <p className="text-xl font-bold select-all">
@@ -26,7 +26,7 @@ export default function Contact({
 </p></div>
         <div>À très vite à la table !<br/><br/></div>
       </div>
-*/
+
    
       <div>  
       <form
