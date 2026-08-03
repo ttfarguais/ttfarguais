6472fc68af1ffa142518c14990428b544d87c378
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 00:14:29 +0200

    Update ButtonGreen.jsx

--- app/src/components/UI/ButtonGreen.jsx
+++ app/src/components/UI/ButtonGreen.jsx
@@ -1,8 +1,27 @@
-import Link from "next/link"
-export default function ButtonGreen({title, url}){
-  return(
-    <button className=" text-contrast-3 py-1 px-4 rounded-xl border border-contrast-3 text-sm hover:bg-solid border-solid hover:text-contrast-1 transition-all">
-    <Link href={url} className="font-bold" aria-label="lire l'article">{title}</Link>
-    </button>
-  )
+import Link from "next/link";
+
+export default function ButtonGreen({ title, url }) {
+  return (
+    <Link
+      href={url}
+      aria-label="lire l'article"
+      className="
+        inline-block
+        py-1
+        px-4
+        rounded-xl
+        border
+        border-solid
+        border-contrast-3
+        text-contrast-3
+        text-sm
+        font-bold
+        hover:bg-solid
+        hover:text-contrast-1
+        transition-all
+      "
+    >
+      {title}
+    </Link>
+  );
 }
