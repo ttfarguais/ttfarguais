2e1d2d7049008c87a592cc666216a29852b8ccd6
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 01:13:03 +0200

    Update ButtonGreen.jsx

--- app/src/components/UI/ButtonGreen.jsx
+++ app/src/components/UI/ButtonGreen.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ButtonGreen({ title, url }) {
   return (
     <Link 
-      href={url}8
+      href={url}
       className="px-4 py-2 bg-green-800 text-white font-bold rounded-md transition-colors duration-300 hover:bg-white hover:text-black border-2 border-green-500 active:bg-white active:text-black"
     >
       {title}
