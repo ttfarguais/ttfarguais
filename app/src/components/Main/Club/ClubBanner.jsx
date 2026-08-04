c1e01bc9e9f40fb15859a7ed4134be7030472db1
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-05 01:14:57 +0200

    Update ClubBanner.jsx

--- app/src/components/Main/Club/ClubBanner.jsx
+++ app/src/components/Main/Club/ClubBanner.jsx
@@ -3,13 +3,13 @@ export default function Cover() {
  
       <section>
         <div
-          className={`relative flex w-full h-56 sm:h-68 md:h-72 lg:h-96  bg-ttfCreate bg-cover bg-center items-center justify-center px-8 py-6 `}
-        >
+  className="relative flex w-full h-56 sm:h-68 md:h-72 lg:h-96 bg-ttfCreate bg-cover bg-center items-end justify-center px-8 pb-8 md:pb-12 lg:pb-16"
+>
           <div
             aria-hidden="true"
             className="absolute z-0 inset-0 w-full bg-black opacity-50 h-56 sm:h-68 md:h-72 lg:h-96"
           ></div>
-          <div className="relative text-contrast-1  flex flex-col justify-center items-center mt-40">
+          <div className="relative text-contrast-1  flex flex-col justify-center items-center">
             <h1 className="text-3xl md:text-5xl uppercase mb-2 font-bold">
               Notre Histoire
             </h1>
