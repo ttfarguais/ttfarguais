22434c7508197afcaf3b7364106bba819420a870
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 02:10:19 +0200

    Update ButtonGreen.jsx

--- app/src/components/UI/ButtonGreen.jsx
+++ app/src/components/UI/ButtonGreen.jsx
@@ -5,7 +5,7 @@ export default function ButtonGreen({ title, url, className = "" }) {
     <Link 
       href={url}
       // On fusionne les classes par défaut avec les classes optionnelles (pour les marges/alignements)
-      className={`px-4 py-2 bg-[#166534] text-white font-bold rounded-md transition-colors duration-300 hover:bg-white hover:text-black hover:border-2 hover:border-[#0E2B1A] active:bg-white active:text-black ${className}`}
+      className={`px-4 py-2 bg-[#166534] text-white font-bold rounded-xl transition-colors duration-300 hover:bg-white hover:text-black hover:border-2 hover:border-[#0E2B1A] active:bg-white active:text-black ${className}`}
     >
       {title}
     </Link>
