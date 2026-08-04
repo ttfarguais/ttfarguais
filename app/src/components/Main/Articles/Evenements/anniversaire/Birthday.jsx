8cbaa8e0422b4403b244ca2b1d75e7f9e19bab0c
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 22:13:44 +0200

    Update Birthday.jsx

--- app/src/components/Main/Articles/Evenements/anniversaire/Birthday.jsx
+++ app/src/components/Main/Articles/Evenements/anniversaire/Birthday.jsx
@@ -6,7 +6,7 @@ export default function Birthday() {
     <section className="mb-16 md:w-4/6 m-auto 2xl:w-3/6">
       <div className="p-6">
         <h1 className="text-xl text-center font-bold p-4 mb-8">
-          Wouah ! 30 ans déjà !
+          Wouah... 30 ans déjà !
         </h1>
 
         <div className="flex flex-col justify-between mb-8 gap-6 text-justify">
