83a69fe3009bda71f4c0f7e543cd1f65cd4bfc29
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 22:31:41 +0200

    Update Training-list.jsx

--- app/src/components/Main/Training/Training-list.jsx
+++ app/src/components/Main/Training/Training-list.jsx
@@ -1,7 +1,7 @@
 export default function TrainingList({category}) {
   return (
     <li className="mb-6">
-    <h2 className="mb-8 text-xl font-extrabold text-solid mt-4">Plage Horaire {category.name}</h2>
+    <h2 className="mb-8 text-xl font-extrabold text-solid mt-4">Plages Horaires {category.name}</h2>
     <ul className="flex flex-col w-full gap-6">
       {category.schedules.map((schedule, index) => (
         <li key={index}>
