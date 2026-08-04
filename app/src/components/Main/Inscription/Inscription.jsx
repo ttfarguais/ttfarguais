2bb1f916e9c579aa1a1b339e63cd6d2358a6ff4a
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-04 23:20:58 +0200

    Update Inscription.jsx

--- app/src/components/Main/Inscription/Inscription.jsx
+++ app/src/components/Main/Inscription/Inscription.jsx
@@ -5,7 +5,7 @@ export default function Inscription({ times, documents, inscriptionFile }) {
     <>
       <section className="p-6 md:w-4/6 m-auto 2xl:w-3/6">
         <h1 className="text-xl text-center font-bold p-4 mb-8">
-          Inscriptions possibles tout au long de la Saison
+          Inscriptions possibles <br/> tout au long de la Saison
         </h1>
 
         <div className="flex flex-col md:flex-row justify-between mb-8 ">
