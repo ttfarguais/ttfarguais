a85b012761191f107eb8609635fe429d881bf999
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-05 01:43:35 +0200

    Update Contact_contacteznous.jsx

--- app/src/components/Main/contact/Contact_contacteznous.jsx
+++ app/src/components/Main/contact/Contact_contacteznous.jsx
@@ -1,1 +1,16 @@
-"use client"; export default function Contact() { return ( <div className="flex flex-col items-center m-auto py-4 px-4"> <h1 className="text-xl text-center font-bold p-4 mb-8"> Contactez-nous ! </h1> <div className="text-center mt-2"> Merci d&apos;envoyer votre demande par mail<br/>à notre Président : </div> <div className="flex flex-col items-center py-2"> <p className="text-xl font-bold select-all"> jeanpaul.vergote@neuf.fr </p> </div> <div className="text-center mt-2"> À très vite à la table ! </div> </div> ); }
+"use client"; export default function Contact()
+  
+{ return ( <div className="flex flex-col items-center m-auto py-4 px-4">
+  <h1 className="text-xl text-center font-bold p-4 mb-8"> Contactez-nous ! </h1>
+  <div className="text-center mt-2"> Merci d&apos;envoyer votre demande par mail
+    <br/>
+    à notre Président : </div> 
+  
+  <div className="flex flex-col items-center py-2"> 
+    <p className="text-xl font-bold select-all"> jeanpaul.vergote@neuf.fr </p> </div> 
+  <div className="text-center mt-2"> 
+    À très vite à la table ! 
+  </div> 
+</div> 
+         ); 
+}
