9e3ef07dc36302a7bb7f2d555786cdd95dd56384
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-05 01:45:34 +0200

    Update Contact_contacteznous.jsx

--- app/src/components/Main/contact/Contact_contacteznous.jsx
+++ app/src/components/Main/contact/Contact_contacteznous.jsx
@@ -1,7 +1,7 @@
 "use client"; export default function Contact()
   
 { return ( <div className="flex flex-col items-center m-auto py-4 px-4">
-  <h1 className="text-xl text-center font-bold p-4 mb-8"> Contactez-nous ! </h1>
+  <h1 className="text-xl text-center font-bold p-4 mb-8"> Envie de nous rej🏓indre ? </h1>
   <div className="text-center mt-2"> Merci d&apos;envoyer votre demande par mail
     <br/>
     à notre Président : </div> 
