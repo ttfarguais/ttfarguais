1e0da1f1d646ae597e1d144141065edfe0102c49
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-05 02:54:20 +0200

    Update Contact_formulaire.jsx

--- app/src/components/Main/contact/Contact_formulaire.jsx
+++ app/src/components/Main/contact/Contact_formulaire.jsx
@@ -1,5 +1,19 @@
 "use client";
+
+import { useState } from "react";
+
 import playerType from "../../../data/playerType";
+
+import {
+  FaUser,
+  FaBirthdayCake,
+  FaEnvelope,
+  FaPhone,
+  FaMapMarkerAlt,
+  FaMedal,
+  FaQuestionCircle,
+  FaCommentDots,
+} from "react-icons/fa";
 
 export default function Contact({
   handleChange,
@@ -9,11 +23,22 @@ export default function Contact({
   validatePhone,
   responseMessage,
   isSubmitted,
-  isValidStatus
+  isValidStatus,
 }) {
 
+  const [touched, setTouched] = useState({});
+  
+  const handleBlur = (e) => {
+  setTouched((prev) => ({
+    ...prev,
+    [e.target.name]: true,
+  }));
+};
+
   const inputClass = (value) =>
-    `w-full rounded-xl border px-4 py-3 bg-white transition focus:outline-none focus:ring-4 focus:ring-green-100 ${
+    `w-full rounded-xl border px-4 py-3 bg-white transition
+    focus:outline-none focus:ring-4 focus:ring-green-100
+    ${
       value
         ? "border-green-500 ring-4 ring-green-100"
         : "border-gray-300 focus:border-green-500"
@@ -32,155 +57,273 @@ export default function Contact({
       {/* Formulaire */}
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-3xl bg-white rounded-3xl shadow-xl p-8 space-y-3"
+        className="w-full max-w-3xl bg-white rounded-3xl shadow-xl p-5 md:p-8"
       >
 
-        {/* Nom / Prénom */}
-        <div className="grid md:grid-cols-3 gap-6">
+        <div className="flex flex-col gap-3">
 
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            placeholder="Votre nom"
-            required
-            className={inputClass(formData.lastName)}
-          />
+          {/* Nom / Prénom */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
 
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            placeholder="Votre prénom"
-            required
-            className={inputClass(formData.firstName)}
-          />
+            <div className="relative">
+              <FaUser
+                className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                  formData.lastName ? "text-green-500" : "text-gray-400"
+                }`}
+              />
 
-        </div>
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Votre nom"
+                required
+                className={`${inputClass(formData.lastName)} pl-12`}
+              />
+              {touched.lastName && !formData.lastName && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+            </div>
 
-        {/* Âge / Email / Téléphone */}
-        <div className="grid md:grid-cols-3 gap-6">
+            <div className="relative">
+              <FaUser
+                className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                  formData.firstName ? "text-green-500" : "text-gray-400"
+                }`}
+              />
 
-          <input
-            type="text"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-            placeholder="Votre âge"
-            required
-            className={inputClass(formData.age)}
-          />
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Votre prénom"
+                required
+                className={`${inputClass(formData.firstName)} pl-12`}
+              />
+              {touched.firstName && !formData.firstName && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+            </div>
 
-          <div>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Votre m@il"
-              required
-              className={inputClass(formData.email)}
-            />
-
-            {isSubmitted && !validateEmail() && (
-              <p className="text-red-600 text-sm mt-2">
-                Adresse e-mail invalide.
-              </p>
-            )}
           </div>
 
-          <div>
-            <input
-              type="tel"
-              name="tel"
-              value={formData.tel}
-              onChange={handleChange}
-              placeholder="Votre téléphone"
-              required
-              className={inputClass(formData.tel)}
+          {/* Age / Email / Téléphone */}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6">
+
+            <div className="relative">
+              <FaBirthdayCake
+                className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                  formData.age ? "text-green-500" : "text-gray-400"
+                }`}
+              />
+
+              <input
+                type="text"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Votre âge"
+                required
+                className={`${inputClass(formData.age)} pl-12`}
+              />
+            </div>
+
+            <div className="relative">
+
+              <FaEnvelope
+                className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                  formData.email ? "text-green-500" : "text-gray-400"
+                }`}
+              />
+
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Votre m@il"
+                required
+                className={`${inputClass(formData.email)} pl-12`}
+              />
+
+             {touched.email && !validateEmail() && (
+                <p className="text-red-600 text-sm mt-2">
+                  Adresse e-mail invalide.
+                </p>
+              )}
+
+            </div>
+
+            <div className="relative">
+
+              <FaPhone
+                className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                  formData.tel ? "text-green-500" : "text-gray-400"
+                }`}
+              />
+
+              <input
+                type="tel"
+                name="tel"
+                value={formData.tel}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                placeholder="Votre téléphone"
+                required
+                className={`${inputClass(formData.tel)} pl-12`}
+              />
+
+            {touched.tel && !formData.tel && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>
+)}
+
+{touched.tel && formData.tel && !validatePhone() && (
+  <p className="text-red-600 text-sm mt-2">
+    Numéro de téléphone invalide.
+  </p>
+)}
+
+            </div>
+
+          </div>          {/* Commune */}
+          <div className="relative">
+
+            <FaMapMarkerAlt
+              className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                formData.municipality ? "text-green-500" : "text-gray-400"
+              }`}
             />
 
-            {isSubmitted && !validatePhone() && formData.tel && (
-              <p className="text-red-600 text-sm mt-2">
-                Numéro de téléphone invalide.
-              </p>
-            )}
+            <input
+              type="text"
+              name="municipality"
+              value={formData.municipality}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Votre commune"
+              required
+              className={`${inputClass(formData.municipality)} pl-12`}
+            />
+            {touched.municipality && !formData.municipality && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+
           </div>
 
-        </div>
+          {/* Type de joueur */}
+          <div className="relative">
 
-        {/* Commune */}
-        <input
-          type="text"
-          name="municipality"
-          value={formData.municipality}
-          onChange={handleChange}
-          placeholder="Votre commune"
-          required
-          className={inputClass(formData.municipality)}
-        />
+            <FaMedal
+              className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                formData.typePlayer ? "text-green-500" : "text-gray-400"
+              }`}
+            />
 
-        {/* Type de joueur */}
-        <select
-          name="typePlayer"
-          value={formData.typePlayer}
-          onChange={handleChange}
-          required
-          className={`w-full rounded-xl border px-4 py-3 bg-white appearance-none transition focus:outline-none focus:ring-4 focus:ring-green-100 ${
-            formData.typePlayer
-              ? "border-green-500 ring-4 ring-green-100 text-gray-900"
-              : "border-gray-300 text-gray-400 focus:border-green-500"
-          }`}
-        >
-          <option value="">Vous êtes un joueur...</option>
+            <select
+              name="typePlayer"
+              value={formData.typePlayer}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              required
+              className={`${inputClass(formData.typePlayer)} pl-12 appearance-none ${
+                formData.typePlayer ? "text-gray-900" : "text-gray-400"
+              }`}
+            >
+              <option value="">Vous êtes un joueur...</option>
 
-          {playerType.map((type, index) => (
-            <option key={index} value={type.title}>
-              {type.title}
-            </option>
-          ))}
-        </select>
+              {playerType.map((type, index) => (
+                <option key={index} value={type.title}>
+                  {type.title}
+                </option>
+              ))}
+            </select>
+{touched.typePlayer && !formData.typePlayer && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+          </div>
 
-        {/* Source */}
-        <input
-          type="text"
-          name="source"
-          value={formData.source}
-          onChange={handleChange}
-          placeholder="Comment avez vous connu le club ?"
-          required
-          className={inputClass(formData.source)}
-        />
+          {/* Source */}
+          <div className="relative">
 
-        {/* Message */}
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          placeholder="Votre demande..."
-          rows={6}
-          required
-          className={`${inputClass(formData.message)} resize-none`}
-        />
+            <FaQuestionCircle
+              className={`absolute left-4 top-1/2 -translate-y-1/2 ${
+                formData.source ? "text-green-500" : "text-gray-400"
+              }`}
+            />
 
-        {responseMessage && (
-          <p
-            className={`rounded-xl p-4 text-white text-center ${
-              isValidStatus ? "bg-solid" : "bg-red-600"
-            }`}
+            <input
+              type="text"
+              name="source"
+              value={formData.source}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Comment avez-vous connu le club ?"
+              required
+              className={`${inputClass(formData.source)} pl-12`}
+            />
+            {touched.source && !formData.source && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+
+          </div>
+
+          {/* Message */}
+          <div className="relative">
+
+            <FaCommentDots
+              className={`absolute left-4 top-5 ${
+                formData.message ? "text-green-500" : "text-gray-400"
+              }`}
+            />
+
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Votre demande..."
+              rows={6}
+              required
+              className={`${inputClass(formData.message)} pl-12 resize-none`}
+            />
+            {touched.message && !formData.message && (
+  <p className="text-red-600 text-sm mt-2">
+    Merci de renseigner.
+  </p>)}
+
+          </div>
+
+          {responseMessage && (
+            <p
+              className={`rounded-xl p-4 text-white text-center ${
+                isValidStatus ? "bg-solid" : "bg-red-600"
+              }`}
+            >
+              {responseMessage}
+            </p>
+          )}
+
+          <button
+            type="submit"
+            className="mx-auto block rounded-full bg-solid text-white px-10 py-3 text-lg font-semibold shadow-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
           >
-            {responseMessage}
-          </p>
-        )}
+            Envoyer m🏓n message
+          </button>
 
-        <button
-          type="submit"
-          className="mx-auto block rounded-full bg-solid text-white px-10 py-2 text-lg font-semibold shadow-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
-        >
-          Envoyer m🏓n message
-        </button>
+        </div>
 
       </form>
 
