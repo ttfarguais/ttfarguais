831d9bd31a3c20b8c00083aed9df22d8b8a5b784
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-07 01:11:38 +0200

    Update AnnouncementCard.jsx

--- app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
+++ app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
@@ -5,36 +5,33 @@ export default function AnnouncementCard({ src, alt, title, description, url, is
   const borderClass = isLast ? "" : "border-b";
 
   return (
-    <article
-      className={`w-full max-w-full flex flex-col sm:flex-row p-4 ${borderClass} md:w-4/6`}
-    >
-      {/* Image */}
-      <div className="block w-full h-48 sm:h-auto sm:w-2/4 relative mb-4 sm:mb-0 flex justify-center">
-        <div className="rounded-xl overflow-hidden">
-          <Image
-            src={src}
-            width={300}
-            height={200}
-            alt={alt}
-            className="rounded-xl object-contain"
-          />
-        </div>
+  <article
+    className={`w-full max-w-full flex flex-col sm:flex-row p-4 ${borderClass} md:w-4/6`}
+  >
+
+    <div className="block w-full h-48 sm:h-auto sm:w-2/4 relative mb-4 sm:mb-0 flex justify-center">
+      <div className="rounded-xl overflow-hidden">
+        <Image
+          src={src}
+          width={200}
+          height={200}
+          alt={alt}
+          className="object-contain rounded-xl"
+        />
       </div>
+    </div>
 
-      {/* Contenu */}
-      <div className="flex flex-col sm:w-2/4 sm:pl-6">
-        <h2 className="font-bold text-lg">
-          {title}
-        </h2>
+    <div className="sm:w-2/4">
+      <h2 className="font-bold">
+        {title}
+      </h2>
 
-        <p className="mt-2">
-          {description}
-        </p>
+      <p>
+        {description}
+      </p>
 
-        <div className="mt-4">
-          <ButtonGreen title="Lire l'article" url={url} />
-        </div>
-      </div>
-    </article>
-  );
-}
+      <ButtonGreen title="Lire l'article" url={url} />
+    </div>
+
+  </article>
+);
