cd56e71f91485a2fdd4102e70523dda6e4cbefa1
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-07 01:14:48 +0200

    Update AnnouncementCard.jsx

--- app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
+++ app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
@@ -2,36 +2,38 @@ import Image from "next/image";
 import ButtonGreen from "../../../UI/ButtonGreen";
 
 export default function AnnouncementCard({ src, alt, title, description, url, isLast }) {
+  // Condition pour déterminer s'il s'agit du dernier élément
   const borderClass = isLast ? "" : "border-b";
 
   return (
-  <article
-    className={`w-full max-w-full flex flex-col sm:flex-row p-4 ${borderClass} md:w-4/6`}
-  >
+    <article
+      className={`w-full max-w-full flex flex-col sm:flex-row p-4 ${borderClass} md:w-4/6`}
+    >
 
-    <div className="block w-full h-48 sm:h-auto sm:w-2/4 relative mb-4 sm:mb-0 flex justify-center">
-      <div className="rounded-xl overflow-hidden">
-        <Image
-          src={src}
-          width={200}
-          height={200}
-          alt={alt}
-          className="object-contain rounded-xl"
-        />
+      <div className="block w-full h-48 sm:h-auto sm:w-2/4 relative mb-4 sm:mb-0 flex justify-center">
+        <div className="rounded-xl overflow-hidden">
+          <Image
+            src={src}
+            width={200}
+            height={200}
+            alt={alt}
+            className="object-contain rounded-xl"
+          />
+        </div>
       </div>
-    </div>
 
-    <div className="sm:w-2/4">
-      <h2 className="font-bold">
-        {title}
-      </h2>
+      <div className="sm:w-2/4">
+        <h2 className="font-bold">
+          {title}
+        </h2>
 
-      <p>
-        {description}
-      </p>
+        <p>
+          {description}
+        </p>
 
-      <ButtonGreen title="Lire l'article" url={url} />
-    </div>
+        <ButtonGreen title="Lire l'article" url={url} />
+      </div>
 
-  </article>
-);
+    </article>
+  );
+}
