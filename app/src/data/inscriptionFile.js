e75cff57f8722f01fb404c1f8b5883dc1e293bab
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-07 01:58:56 +0200

    Update inscriptionFile.js

--- app/src/data/inscriptionFile.js
+++ app/src/data/inscriptionFile.js
@@ -4,13 +4,10 @@ const times = [
 ]
 
 const documents = [
-    { name: "Parcours de Prévention Santé (PPS)", pdf: "https://malicence.fftt.com",
-      tutorial: "https://ton-lien-de-tutoriel.fr"
-  },
-  { 
-    name: "Bordereau de Licence", 
-    pdf: "/pdf/licence.pdf" 
-  },
+   { name: "Bordereau de Licence", pdf: "/pdf/licence.pdf" },
+   { name: "Parcours de Prévention Santé (PPS)", pdf: "https://malicence.fftt.com",
+      tutorial: "https://ton-lien-de-tutoriel.fr"},
+
   { name: "Bordereau de Licence", pdf: "/pdf/licence.pdf" },
   { name: "Fiche de renseignements Jeunes", pdf: "/pdf/TTF-Fiche-renseignements-jeunes.pdf" },
   {
