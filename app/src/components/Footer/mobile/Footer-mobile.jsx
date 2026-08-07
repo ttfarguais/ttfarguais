2ee91ad55a82213b61b5d8cb9d70a18cfab19dc9
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-07 22:46:36 +0200

    Update Footer-mobile.jsx

--- app/src/components/Footer/mobile/Footer-mobile.jsx
+++ app/src/components/Footer/mobile/Footer-mobile.jsx
@@ -6,10 +6,9 @@ import FooterSponsorMobile from "./Footer-SponsorMobile";
 export default function FooterMobile() {
   return (
     <div className="flex lg:hidden flex-col items-center gap-10 justify-center bg-solid text-contrast-1 px-8 pb-6 pt-10">
-   <div className="flex flex-col items-center gap-6 w-full">
+
          <FooterAdress />
-       </div>
-      <div className="flex-1 flex justify-center">
+    
         <FooterSocialLinks />
       </div>
   </div>
