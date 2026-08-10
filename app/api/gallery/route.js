b81e9fd031994a336ced45281bf36a4120ce8542
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-10 23:48:33 +0200

    Update route.js

--- app/api/gallery/route.js
+++ app/api/gallery/route.js
@@ -1,11 +1,23 @@
 export const dynamic = "force-dynamic";
+
 import { list } from "@vercel/blob";
 
 export async function GET(request) {
-  const { blobs } = await list(); 
+  const { blobs } = await list();
 
-  const files = blobs.filter((blob) =>
-    blob.pathname.includes("galerie/") && (blob.pathname.includes(".JPG") || blob.pathname.includes(".jpg") || blob.pathname.includes(".jpeg") || blob.pathname.includes(".JPEG") || blob.pathname.includes(".png") || blob.pathname.includes(".PNG"))
-  );
+  const files = blobs
+    .filter((blob) =>
+      blob.pathname.includes("galerie/") &&
+      (
+        blob.pathname.includes(".JPG") ||
+        blob.pathname.includes(".jpg") ||
+        blob.pathname.includes(".jpeg") ||
+        blob.pathname.includes(".JPEG") ||
+        blob.pathname.includes(".png") ||
+        blob.pathname.includes(".PNG")
+      )
+    )
+    .sort((a, b) => new Date(b.uploadedAt) - new Date(a.uploadedAt));
+
   return Response.json(files);
 }
