5e8138cc1bdbdea4af5c4fc5ccc19b80d66864f1
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-16 22:18:40 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -2,18 +2,19 @@ const trainingTimes = [
   {
     name: "Adultes",
     schedules: [
-      { day: "Lundi", time: "18h00 - 20h30", who: "Loisirs et compétiteurs", trainingType: "Jeux libre" },
-      { day: "Mercredi", time: "19h00 - 20h00", trainingType: "Entraînement dirigé : 10 joueurs", coach: "Maxime TOURNAUX" },
+      { day: "Lundi", time: "18h00 - 20h30", who: "Loisirs et compétiteurs"},
       { day: "Mercredi", time: "19h00 - 21h00", trainingType: "Jeux libre" },
+      { day: "Mercredi", time: "19h00 - 20h00", trainingType: "Entraînement dirigé : 12 joueurs", coach: "Maxime TOURNAUX" },
+ 
     ]
 },
   {
-    name: "Jeunes",
+    name: "Spécifiques Jeunes le Mercredi",
     schedules: [
 
-      { day: "Mercredi", time: "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
-      { day: "Mercredi", time: "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
-      { day: "Mercredi", time: "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
+      { time: "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*" },
+      { time: "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*" },
+      { time: "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
     ]
   }
 ];
