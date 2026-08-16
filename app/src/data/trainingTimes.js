95e06bba6c5a2616f37721cdf517099670078584
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-17 01:55:57 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -13,7 +13,7 @@ const trainingTimes = [
     schedules: [
 
       { coach: "Maxime TOURNAUX", who: "Mercredi : Entraînement dirigé" },
-      { time: "14h00 - 15h30", day: "Initiation écoles primaires : Groupe 1*" },
+      { time: "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*" },
       { time: "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*" },
       { time: "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*" },
     ]
