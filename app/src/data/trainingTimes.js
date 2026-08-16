b89ac28bc095a13afe760cc8f1d34422289295de
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-17 00:59:04 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -2,9 +2,9 @@ const trainingTimes = [
   {
     name: "Adultes",
     schedules: [
-      { day: "Lundi :", time: "18h00 - 20h30 -> Jeux Libre", who: "Loisirs et compétiteurs", trainingType: "Jeux libre" },
-      { day: "Mercredi :", time: "19h00 - 21h00", trainingType: "Jeux libre" },
-      { day: "Mercredi :", time: "19h00 - 20h00", trainingType: "Entraînement dirigé : 10 joueurs", coach: "Maxime TOURNAUX" },
+      { day: "Lundi :", time: "18h00 - 20h30 -> Jeux Libre", who: "Loisirs et compétiteurs" },
+      { day: "Mercredi :", time: "19h00 - 21h00 -> Jeux Libre" },
+      { day: "Mercredi :", time: "19h00 - 20h00", trainingType: "Entraînement dirigé : 12 joueurs", coach: "Maxime TOURNAUX" },
  
     ]
 },
@@ -12,7 +12,7 @@ const trainingTimes = [
     name: "Jeunes le Mercredi",
     schedules: [
 
-      { day: "Entraînement dirigé :", time: "Maxime TOURNAUX" },
+      { day: "Entraînement dirigé :", time: "Maxime TOURNAUX", who: "Mercredi" },
       { time: "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*" },
       { time: "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*" },
       { time: "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*" },
