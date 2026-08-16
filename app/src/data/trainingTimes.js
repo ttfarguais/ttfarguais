aaf9dbe151f5e4c853e8714f4a68c81c553f99ac
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-16 22:27:40 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -12,9 +12,9 @@ const trainingTimes = [
     name: "Spécifiques Jeunes le Mercredi",
     schedules: [
 
-      { time: "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*" },
-      { time: "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*" },
-      { time: "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
+      { day "14h00 - 15h30", who: "Initiation écoles primaires : Groupe 1*" },
+      { day "15h30 - 17h00", who: "Débutants collèges-lycées : Groupe 2*" },
+      { day "17h00 - 19h00", who: "Confirmés collèges-lycées : Groupe 3*", trainingType: "Entraînement dirigé", coach: "Maxime TOURNAUX" },
     ]
   }
 ];
