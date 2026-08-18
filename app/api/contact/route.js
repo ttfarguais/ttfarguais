fc3dc1934c1278f452fbe8ac9753f49e58dbb2d5
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-19 00:53:28 +0200

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
   const mailOptions = {
     from: email,
     to: `${process.env.USER_EMAIL},${process.env.USER_EMAIL1}`,
-    subject: `Contact du site TTF`,
+    subject: `Contact du site TTFARGUAIS`,
     text: `
       Nom: ${lastName}
       Prénom: ${firstName}
