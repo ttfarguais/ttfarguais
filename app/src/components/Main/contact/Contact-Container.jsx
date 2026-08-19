87b0f565bb217c05f6e8365cc10ea98b778103f0
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-19 23:25:06 +0200

    Update Contact-Container.jsx

--- app/src/components/Main/contact/Contact-Container.jsx
+++ app/src/components/Main/contact/Contact-Container.jsx
@@ -64,7 +64,7 @@ export default function ContactContainer() {
       const data = await res.json();
       setIsValidStatus(true);
       setResponseMessage(data.message);
-      setIsFormVisible(false);
+  //    setIsFormVisible(false);
       
       setFormData({
         lastName: "",
