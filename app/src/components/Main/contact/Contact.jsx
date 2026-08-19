70d30cf9ad3cb94b25cfe989ca9da1d51179a73d
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-19 02:19:27 +0200

    Update Contact.jsx

--- app/src/components/Main/contact/Contact.jsx
+++ app/src/components/Main/contact/Contact.jsx
@@ -85,20 +85,13 @@ export default function Contact({
         À très vite à la table !
       </p>
     </div>*/}
-     
       {/* Formulaire */}
- <form
-  onSubmit={handleSubmit}
-  className="w-full max-w-3xl bg-white rounded-3xl shadow-xl p-5 md:p-8"
->
-  {isSubmitted ? (
-    <div className="text-center py-8">
-      <p className="text-green-600 font-bold text-lg">
-        {responseMessage}
-      </p>
-    </div>
-  ) : (
-    <div className="flex flex-col gap-5">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-3xl bg-white rounded-3xl shadow-xl p-5 md:p-8"
+      >
+
+        <div className="flex flex-col gap-5">
 
           {/* Nom / Prénom */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
@@ -361,16 +354,16 @@ export default function Contact({
           )}
 
           <button
-          type="submit"
-          className="mx-auto block rounded-full bg-solid text-white px-10 py-3 text-lg font-semibold shadow-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
-        >
-          Envoyer m🏓n message
-        </button>
+            type="submit"
+            className="mx-auto block rounded-full bg-solid text-white px-10 py-3 text-lg font-semibold shadow-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl"
+          >
+            Envoyer m🏓n message
+          </button>
 
-       </div>
-  )}
+        </div>
 
-</form>
+      </form>
 
-</div>
-);
+    </div>
+  );
+}
