161699724ff1248178b42e311326e04f0af4486d
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-19 22:31:12 +0200

    Update Contact-Container.jsx

--- app/src/components/Main/contact/Contact-Container.jsx
+++ app/src/components/Main/contact/Contact-Container.jsx
@@ -31,7 +31,6 @@ export default function ContactContainer() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsSubmitted(true);
     const emailValid = isValidEmail(formData.email);
     const phoneValid = isValidPhoneNumber(formData.tel);
 
@@ -63,6 +62,8 @@ export default function ContactContainer() {
       const data = await res.json();
       setIsValidStatus(true);
       setResponseMessage(data.message);
+      setIsSubmitted(true);
+      
       setFormData({
         lastName: "",
         firstName: "",
@@ -77,6 +78,8 @@ export default function ContactContainer() {
     } catch (error) {
       console.error("Erreur:", error);
       setIsValidStatus(false);
+    setIsSubmitted(false);
+      
       setResponseMessage("Le formulaire n'a pas été transmis.\n" +
                          "Merci d'envoyer un mail à notre Président :\n\n" +                        
                          "jeanpaul.vergote@neuf.fr\n" +
