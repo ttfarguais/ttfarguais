ed06bee4193c02a8efb7bbcc11274676a5a3580b
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-21 02:07:59 +0200

    Update Cover.jsx

--- app/src/components/Main/Home/Cover.jsx
+++ app/src/components/Main/Home/Cover.jsx
@@ -51,7 +51,7 @@ export default function Cover() {
                                 <a
                                     href={cacheBustingUrl(newsFile)}
                                     target="_blank"
-                                    className={`py-2 px-4 border rounded-xl hover:bg-white hover:text-black text-sm transition-all animate-pulse ${
+                                    className={`py-2 px-4 border rounded-xl hover:bg-white hover:text-black text-sm transition-all animate-[pulse_1.2s_ease-in-out_infinite] shadow-lg ${
     newsFile
         ? "text-white border-white"
         : "border-gray-400 text-gray-400"
