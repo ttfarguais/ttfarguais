a4642b149de42e39b9cf53fc2d32d000250766e2
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-21 02:44:55 +0200

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -19,8 +19,8 @@ export async function POST(request) {
 
   const mailOptions = {
     from: email,
-    // to: `${process.env.USER_EMAIL},${process.env.USER_EMAIL1},${process.env.USER_EMAIL2},${process.env.USER_EMAIL3}`,
-    to: `${process.env.USER_EMAIL1}`,
+    to: `${process.env.USER_EMAIL},${process.env.USER_EMAIL1},${process.env.USER_EMAIL2},${process.env.USER_EMAIL3}`,
+     //  to: `${process.env.USER_EMAIL1}`, //
     subject: `Nouveau Contact du Site TTFARGUAIS`,
 
     text: `
