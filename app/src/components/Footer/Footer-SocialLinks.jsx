a5702628c7385e58db6b0be13a3ba6c01c94ab9a
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-23 23:48:47 +0200

    Update Footer-SocialLinks.jsx

--- app/src/components/Footer/Footer-SocialLinks.jsx
+++ app/src/components/Footer/Footer-SocialLinks.jsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
 import { FacebookLink, InstagramLink } from "../UI/Icons";
+import { MdAlternateEmail } from "react-icons/md";
+
 export default function FooterSocialLinks() {
   return (
     <div>
       <ul className="flex gap-8 justify-center">
         <li className="w-8 h-8">
-        <FacebookLink />
+          <FacebookLink />
         </li>
+
         <li className="w-8 h-8">
-       <InstagramLink />
+          <InstagramLink />
+        </li>
+
+        <li className="w-8 h-8">
+          <Link href="/contact" aria-label="Nous contacter">
+            <MdAlternateEmail className="w-8 h-8" />
+          </Link>
         </li>
       </ul>
     </div>
