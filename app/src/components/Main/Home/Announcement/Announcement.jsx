88c3c286d79213a40c203a24cc942709a305e9b0
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-24 00:25:52 +0200

    Update Announcement.jsx

--- app/src/components/Main/Home/Announcement/Announcement.jsx
+++ app/src/components/Main/Home/Announcement/Announcement.jsx
@@ -2,22 +2,20 @@ import AnnouncementCard from "./AnnouncementCard";
 
 import newsArticles from "../../../../data/newsArticles";
 
-
 export default function Announcement() {
   return (
     <section className="bg-contrast-2 border-t flex flex-col items-center py-6">
       {newsArticles.map((announcement, index) => (
-        <AnnouncementCard 
+        <AnnouncementCard
           src={announcement.src}
           key={index}
           url={announcement.url}
           alt={announcement.alt}
           title={announcement.title}
           description={announcement.description}
-          isLast={index === announcement.length - 1}
+          isLast={index === newsArticles.length - 1}
         />
       ))}
     </section>
   );
 }
-
