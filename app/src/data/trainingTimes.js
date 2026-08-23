dbaf4229ceaf9acaabd037ce8fbabb5fdf58af4c
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-23 20:44:17 +0200

    Update trainingTimes.js

--- app/src/data/trainingTimes.js
+++ app/src/data/trainingTimes.js
@@ -2,14 +2,14 @@ const trainingTimes = [
   {
     name: "Adultes",
     schedules: [
-      { day: "Lundi :", time: "18h00 - 20h30 -> Jeux Libre", who: "Loisirs et compétiteurs" },
+      { day: "Lundi :", time: "18h00 - 20h30 -> Jeux Libre", who: "Pour Tous" },
       { day: "Mercredi :", time: "19h00 - 21h00 -> Jeux Libre" },
       { day: "Mercredi :", time: "19h00 - 20h00", trainingType: "Entraînement dirigé : 12 joueurs", coach: "Maxime TOURNAUX" },
  
     ]
 },
   {
-    name: "Jeunes",
+    name: "Spécifique Jeunes",
     schedules: [
 
       { coach: "Maxime TOURNAUX", who: "Mercredi : Entraînement dirigé" },
