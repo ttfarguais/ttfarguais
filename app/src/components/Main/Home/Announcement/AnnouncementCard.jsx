cf2a32b1ff8d599a1dfc619e0805f56f688e97a9
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-24 00:59:08 +0200

    Update AnnouncementCard.jsx

--- app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
+++ app/src/components/Main/Home/Announcement/AnnouncementCard.jsx
@@ -15,9 +15,9 @@ export default function AnnouncementCard({
     <article
       className={`w-full max-w-full flex flex-col sm:flex-row items-center p-4 pb-8 ${borderClass} md:w-4/6`}
     >
-      <div className="block w-full h-48 sm:h-auto sm:w-2/4 relative mb-4 sm:mb-0">
+      <div className="w-full sm:w-2/4 flex justify-center mb-4 sm:mb-0">
         <Image
-          className="rounded-xl object-contain"
+          className="w-full max-w-[500px] h-auto rounded-xl object-contain"
           src={src}
           alt={alt}
           width={500}
@@ -25,10 +25,14 @@ export default function AnnouncementCard({
         />
       </div>
 
-      <div className="p-4 sm:w-2/4">
-        <h3 className="font-bold uppercase mb-4">{title}</h3>
+      <div className="w-full sm:w-2/4 p-4">
+        <h3 className="font-bold uppercase mb-4">
+          {title}
+        </h3>
 
-        <p className="text-sm mb-4 text-justify">{description}</p>
+        <p className="text-sm mb-4 text-justify">
+          {description}
+        </p>
 
         <div className="mt-6">
           <ButtonGreen title="En savoir plus" url={url} />
