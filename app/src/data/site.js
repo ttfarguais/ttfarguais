cb82a510761bc97b49f31199211c07fe69a681d5
TT Farguais <tenisdetablefarguais@gmail.com>
2026-08-24 01:16:15 +0200

    Update site.js

--- app/src/data/site.js
+++ app/src/data/site.js
@@ -1,22 +1,18 @@
 const siteLinks = [
   { title: "Accueil", url: "/" },
-  {
-    title: "Le club",
-    url: "/leclub",
-    subLinks: [
-      
+  { title: "Le club", url: "/leclub",
+   
+    subLinks: [ 
       { title: "Le bureau", url: "/bureau" },
       { title: "Tournois", url: "/tournois" },
       { title: "Palmarès", url: "/palmares" },
       { title: "Galerie", url: "/galerie" },
     ],
   },
+  
   { title: "Entraînements", url: "/entrainements" },
   { title: "Inscription", url: "/inscription" },
-  { title: "Compétition", url: "/competition" },
-  
-    {/*    subLinks: [{ title: "Compétiteurs", url: "/competiteurs" }], */}
-  
+  { title: "Compétition", url: "/competition" },  
   { title: "Contact", url: "/contact" },
 ];
 
